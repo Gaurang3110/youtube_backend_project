918b20bb1e63fe58993d2da4182a77bce243f9f6
Gaurang Srivastava <90106787+Gaurang3110@users.noreply.github.com>
2025-09-25 01:04:28 +0530

    utils

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+//global event handler
+app.on("error", (error) => {
+  console.error("ERROR: ", error);
+  throw error; 
+});
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
 
-export {app};
+app.use(express.json({limit: "!6kb"}))
+
+//for taking data from url
+app.use(express.urlencoded({
+  extended:true,
+  limit: "16kb"
+}))
+
+//to store public assets (public folder)
+app.use(express.static("public"))
+
+app.use(cookieParser());
+
+export default app;
